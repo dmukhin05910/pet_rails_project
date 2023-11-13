d55cb50eab4489539a1149f52aa756631f7d289e
dmukhin05910 <dmukhin@teladoc.com>
2023-11-13 17:24:25 +0000

    Associations for table

--- app/models/employee_territory.rb
+++ app/models/employee_territory.rb
@@ -1,2 +1,4 @@
 class EmployeeTerritory < ApplicationRecord
+  belongs_to :employee
+  belongs_to :territory
 end
--- app/models/territory.rb
+++ app/models/territory.rb
@@ -1,2 +1,4 @@
 class Territory < ApplicationRecord
+  has_many :employee_territories
+  belongs_to :region
 end
